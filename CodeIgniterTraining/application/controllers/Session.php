058190ca3b6ef9f7b00ad3d48ab6c3c06b7d5b40
sam <siewkingsam@gmail.com>
2024-01-07 14:35:08 +0800

    try html css coding

---
+++ CodeIgniterTraining/application/controllers/Session.php
@@ -0,0 +1,33 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Session extends CI_Controller {
+
+	/**
+	 * Index Page for this controller.
+	 *
+	 * Maps to the following URL
+	 * 		http://example.com/index.php/welcome
+	 *	- or -d
+	 * 		http://example.com/index.php/welcome/index
+	 *	- or -
+	 * Since this controller is set as the default controller in
+	 * config/routes.php, it's displayed at http://example.com/
+	 *
+	 * So any other public methods not prefixed with an underscore will
+	 * map to /index.php/welcome/<method_name>
+	 * @see https://codeigniter.com/userguide3/general/urls.html
+	 */
+	public function index()
+	{
+		$this->load->view('session_index');
+	}
+	public function login_admin()
+	{
+        echo 'login for admin';
+	}
+    public function login_student()
+	{
+        $this->load->view('login');
+	}
+}
--- CodeIgniterTraining/application/models/Session_model.php
+++ CodeIgniterTraining/application/models/Session_model.php
@@ -2,44 +2,84 @@
 class Session_model extends CI_Model
 {
 
+    private $session_ID;
+    private $session_name;
+    private $session_status;
+    private $application_type;
+    private $startdate;
+    private $end_date;
 
-    // public $title;
-    // public $content;
-    // public $date;
-
-    public function get_all_students()
+    public function set_session_id($session_ID){
+        $this->session_ID = $session_ID;
+    }
+    public function get_session_id(){
+        return $this->session_ID;
+    }
+    public function set_session_name($session_name){
+        $this->session_name = $session_name;
+    }
+    public function get_session_name(){
+        return $this->session_name;
+    }
+    public function set_session_status($session_status){
+        $this->session_status = $session_status;
+    }
+    public function get_session_status(){
+        return $this->session_status;
+    }
+    public function set_application_type($application_type){
+        $this->application_type = $application_type;
+    }
+    public function get_application_type(){
+        return $this->application_type;
+    }
+    public function set_start_date($startdate){
+        $this->startdate = $startdate;
+    }
+    public function get_start_date(){
+        return $this->startdate;
+    }
+    public function set_end_date($end_date){
+        $this->end_date = $end_date;
+    }
+    public function get_end_date(){
+        return $this->end_date;
+    }
+    public function get_all_session()
     {
-        //run query to get all student records from db
-        $query = $this->db->get('student_profile');
+        // query to get all session records
+        $query = $this->db->get('session');
         return $query;
     }
 
-    public function delete_student_id($id_pelajar)
+    public function delete_session_id($session_ID)
     {
 
         //delete query prep and having pipeline to specify which to delete
         $query = $this->db
-            ->where("id_pelajar", $id_pelajar)
-            ->delete('student_profile');
+            ->where("session_ID", $session_ID)
+            ->delete('session');
 
         //return query to execute
         return $query;
     }
 
-    public function save_student(){
+    public function save_session(){
 
-        $this->db->set("NAMA_PELAJAR",$this->input->post("name"));
-        $this->db->set("PROGRAM",$this->input->post("program"));
-        $this->db->set("ICNO",$this->input->post("icno"));
+        $this->db->set("SESSION_NAME",$this->input->post("session_name"));
+        $this->db->set("SESSION_STATUS",$this->input->post("status"));
+        $this->db->set("APPLICATION_TYPE",$this->input->post("application_type"));
+        $this->db->set("START_DATE",$this->input->post("start_date"));
+        $this->db->set("END_DATE",$this->input->post("end_date"));
         
-        $this->db->insert('student_profile');
+        $this->db->insert('application_session');
     }
 
-    public function get_student($id_pelajar)
+    public function get_session($session_ID)
     {
         $query = $this->db
-        ->where("id_pelajar",$id_pelajar)
-        ->get('student_profile');
+        ->where("session_ID",$session_ID)
+        ->get('application_session');
         
         return $query;
     }
