c1dc3fc444e60521159ddd80411b294c5b1eb4b1
syfqhchwn <121805874+syfqhchwn@users.noreply.github.com>
2024-04-07 04:03:27 +0800

    login ui

--- CodeIgniterTraining/application/views/login_view.php
+++ CodeIgniterTraining/application/views/login_view.php
@@ -1,23 +1,78 @@
 <!-- login_view.php -->
 
 <!DOCTYPE html>
-<html lang="en">
-
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
-</head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Login Page</title>
+  <!-- Tell the browser to be responsive to screen width -->
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+  <!-- Bootstrap 3.3.7 -->
+  <link rel="stylesheet" href="<?php echo base_url() ?>html_ref/bower_components/bootstrap/dist/css/bootstrap.min.css">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="<?php echo base_url() ?>html_ref/bower_components/font-awesome/css/font-awesome.min.css">
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="<?php echo base_url() ?>html_ref/bower_components/Ionicons/css/ionicons.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="<?php echo base_url() ?>html_ref/dist/css/AdminLTE.min.css">
+  <!-- iCheck -->
+  <link rel="stylesheet" href="<?php echo base_url() ?>html_ref/plugins/iCheck/square/blue.css">
 
-<body>
-    <h2>Login</h2>
+  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+  <!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
+
+  <!-- Google Font -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+</head>
+<body class="hold-transition login-page">
+<div class="login-box">
+  <div class="login-logo">
+    <a href="<?php echo base_url() ?>html_ref/index2.html"><b>Hostel</b>UMT</a>
+  </div>
+  <!-- /.login-logo -->
+  <div class="login-box-body">
+    <p class="login-box-msg">Sila Log Masuk</p>
+
     <?php if (isset($error)) { ?>
         <div style="color: red;"><?php echo $error; ?></div>
     <?php } ?>
     <form action="<?php echo base_url('CodeIgniterTraining/index.php/logincontroller/login'); ?>" method="post">
-        No Matrik <input type="text" name="student_id"><br>
-        <input type="submit" value="Login">
+      <div class="form-group has-feedback">
+        <input type="text" class="form-control" placeholder="No Matrik" name="student_id">
+        <span class="glyphicon glyphicon-user form-control-feedback"></span>
+      </div>
+      <div class="row">
+        <!-- Move the button to the right side -->
+        <div class="col-xs-8"></div>
+        <div class="col-xs-4">
+          <button type="submit" class="btn bg-purple btn-block btn-flat">Log Masuk</button>
+        </div>
+      </div>
     </form>
-</body>
+  </div>
+  <!-- /.login-box-body -->
+</div>
+<!-- /.login-box -->
 
+<!-- jQuery 3 -->
+<script src="<?php echo base_url() ?>html_ref/bower_components/jquery/dist/jquery.min.js"></script>
+<!-- Bootstrap 3.3.7 -->
+<script src="<?php echo base_url() ?>html_ref/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+<!-- iCheck -->
+<script src="<?php echo base_url() ?>html_ref/plugins/iCheck/icheck.min.js"></script>
+<script>
+  $(function () {
+    $('input').iCheck({
+      checkboxClass: 'icheckbox_square-blue',
+      radioClass: 'iradio_square-blue',
+      increaseArea: '20%' /* optional */
+    });
+  });
+</script>
+</body>
 </html>
