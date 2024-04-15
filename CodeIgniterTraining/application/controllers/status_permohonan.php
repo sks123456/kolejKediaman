e5b2b93fbf8fec45460f74857bce1415e7d3b034
sam <siewkingsam@gmail.com>
2024-04-15 15:53:53 +0800

    Revert "s"
    This reverts commit 632943ec6dff3b6bf4166a8a198c0e816af89c83.

--- CodeIgniterTraining/application/controllers/status_permohonan.php
+++ CodeIgniterTraining/application/controllers/status_permohonan.php
@@ -3,37 +3,18 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class status_permohonan extends CI_Controller
 {
-    public function __construct()
-    {
-        parent::__construct();
-        $this->load->library('session');
-    }
-
-    public function index($student_id)
+    public function index()
     {
         $this->load->helper('url');
         $this->load->model("Session_model");
-        $this->load->model("Student_model");
-        $this->load->model("Application_model");
 
-        $sessions = $this->Session_model->get_active_session()->row_array();
-        $student_data = $this->Student_model->get_student($student_id)->row_array();
-        // Fetch multiple application records for the student
-        $applications = $this->db->query("
-        SELECT *
-        FROM application AS a
-        JOIN student_profile AS s ON a.stud_matric = s.stud_matric
-        JOIN kk_session AS c ON a.session_id = c.session_id
-        JOIN kk_channel AS k ON a.channel_id = k.channel_id
-        LEFT JOIN kk_uniform AS u ON a.UNIT_ID = u.UNIFORM_ID
-        WHERE a.stud_matric = '$student_id'
-    ")->result_array();
-        // Pass the session data and applications array to the view
+        $sessions = $this->Session_model->get_session_id_and_name();
+
+        // Pass the session data to the view
         $data['sessions'] = $sessions;
-        $data['student_data'] = $student_data;
-        $data['applications'] = $applications;
-
-        $this->load->model("application_model");
+        
+        $this->load->model("application_model");    
         $this->load->view('stud_status', $data);
     }
+
 }
