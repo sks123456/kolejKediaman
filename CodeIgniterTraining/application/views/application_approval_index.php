c77995a34adbce6fa243452ef96a2c083a9bc850
sam <siewkingsam@gmail.com>
2024-06-11 02:09:02 +0800

    hide filter application

--- CodeIgniterTraining/application/views/application_approval_index.php
+++ CodeIgniterTraining/application/views/application_approval_index.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+!DOCTYPE html>
 <html lang="en" dir="ltr" data-bs-theme="light" data-color-theme="Blue_Theme" data-layout="vertical">
 
 <head>
@@ -51,26 +51,40 @@
                     </div>
                     <div class="row">
                         <div class="card w-100 position-relative overflow-hidden">
-                            <div class="px-4 py-3 border-bottom">
-                                <?php $this->load->view("kelulusan_form"); ?>
+                            <div class="px-4 py-3 border-bottom d-flex justify-content-between align-items-center">
+                                <h4 class="me-3 mb-0">List Of Application</h4>
+                                <button class="btn btn-primary ms-auto" data-bs-toggle="modal" data-bs-target="#filterModal">Filter</button>
                             </div>
                         </div>
                     </div>
+
+                    <!-- Filter Form Modal -->
+                    <div class="modal fade" id="filterModal" tabindex="-1" aria-labelledby="filterModalLabel" aria-hidden="true">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id="filterModalLabel">Filter Form</h5>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body">
+                                    <?php $this->load->view("kelulusan_form"); ?>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
                     <!-- Row -->
                     <div class="row">
                         <!-- Column -->
                         <div class="col-lg-12">
                             <div class="card w-100 position-relative overflow-hidden">
                                 <div class="px-4 py-3 border-bottom">
-                                    <h4 class="card-title mb-0">List of Registration</h4>
                                     <div class="table-responsive mb-4">
                                         <?php $this->load->view("application_list"); ?>
                                     </div>
                                 </div>
 
                             </div>
-
-
                         </div>
                     </div>
                 </div>
